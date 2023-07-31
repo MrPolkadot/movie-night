50687bd02d69f6340a0971fe08270a7c9be708e7
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-07-30 19:08:35 -0600

    Add a dropdown menu and event listener for the menu.Add some variables

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
+let lastThreePicks = document.querySelector(".picks");
+let randomMovie = document.querySelector(".random");
+let randomMovieImage = document.querySelector(".random-movie-image");
+
+
+var dropdown = document.querySelector('.dropdown');
+dropdown.addEventListener('click', function(event) {
+  event.stopPropagation();
+  dropdown.classList.toggle('is-active');
+});
+
+
+
+
+
 let watchModeApiKey = "UZsT73vA3Tb6Sarx9DpPKtRWdc3u4qXCdT9vB3Zp"; //Key works!!
 
 
